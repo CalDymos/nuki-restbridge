33fbb602a7096b2c2a4a07f60b1228457fc8914f
CalDymos <T.Sender@gmx.de>
2025-03-12 23:38:39 +0100

    update WebCfgServer

--- src/WebCfgServer.cpp
+++ src/WebCfgServer.cpp
@@ -30,7 +30,6 @@ WebCfgServer::WebCfgServer(NukiWrapper *nuki, NukiNetwork *network, Preferences 
         str = _preferences->getString(preference_webcfgserver_cred_password, "");
         const char *pass = str.c_str();
         memcpy(&_credPassword, pass, str.length());
-
     }
     _confirmCode = generateConfirmCode();
 }
@@ -41,21 +40,179 @@ WebCfgServer::~WebCfgServer()
     delete _webServer;
 }
 
+bool WebCfgServer::isAuthenticated(WebServer *server)
+{
+    String cookieKey = "sessionId";
+
+    // Hole alle Cookies als String
+    String cookieHeader = server->header("Cookie");
+
+    if (cookieHeader.length() > 0)
+    {
+        // Suche den Cookie-Wert
+        int startIndex = cookieHeader.indexOf(cookieKey + "=");
+        if (startIndex != -1)
+        {
+            startIndex += cookieKey.length() + 1; // Position nach dem Gleichheitszeichen
+            int endIndex = cookieHeader.indexOf(";", startIndex);
+            if (endIndex == -1)
+                endIndex = cookieHeader.length();
+
+            String cookie = cookieHeader.substring(startIndex, endIndex);
+
+            if (_httpSessions[cookie].is<JsonVariant>())
+            {
+                struct timeval time;
+                gettimeofday(&time, NULL);
+                int64_t time_us = (int64_t)time.tv_sec * 1000000L + (int64_t)time.tv_usec;
+
+                if (_httpSessions[cookie].as<signed long long>() > time_us)
+                {
+                    return true;
+                }
+                else
+                {
+                    Log->println("Cookie found, but not valid anymore");
+                }
+            }
+        }
+    }
+    return false;
+}
+
+int WebCfgServer::doAuthentication(WebServer *server)
+{
+    if (!_network->isApOpen() && _preferences->getString(preference_bypass_proxy, "") != "" && server->client().localIP().toString() == _preferences->getString(preference_bypass_proxy, ""))
+    {
+        return 4;
+    }
+    else if (strlen(_credUser) > 0 && strlen(_credPassword) > 0)
+    {
+        int savedAuthType = _preferences->getInt(preference_http_auth_type, 0);
+        if (savedAuthType == 2)
+        {
+            if (!isAuthenticated(server))
+            {
+                Log->println("Authentication Failed");
+                return savedAuthType;
+            }
+        }
+        else
+        {
+            if (!server->authenticate(_credUser, _credPassword))
+            {
+                Log->println("Authentication Failed");
+                return savedAuthType;
+            }
+        }
+    }
+
+    return 4;
+}
+
 void WebCfgServer::initialize()
 {
-    // Route für GET-Anfrage auf "/" (z.B. Einstellungsformular)
-    _webServer->on("/", HTTP_GET, [this]()
-                   { handleRoot(); });
 
-    // Route für POST-Anfrage auf "/save" (z.B. zum Speichern der Settings)
-    _webServer->on("/save", HTTP_POST, [this]()
-                   { handleSave(); });
-
-    // Route für css Style 
-    _webServer->on("/style.css", [this]() { handleCSS();});
+    // Route für css Style
+    _webServer->on("/style.css", HTTP_GET, [this]()
+                   { sendCss(this->_webServer); });
 
     _webServer->onNotFound([this]()
-                           { handleNotFound(); }); // 404-Handler setzen
+                           { redirect(this->_webServer, "/"); });
+
+    if (_network->isApOpen())
+    {
+
+        _webServer->on("/ssidlist", HTTP_GET, [this]()
+                       { buildSSIDListHtml(this->_webServer); });
+
+        _webServer->on("/savewifi", HTTP_POST, [this]()
+                       {            int authReq = doAuthentication(this->_webServer);
+                       
+                                   switch (authReq)
+                                   {
+                                       case 0:
+                                           return this->_webServer->requestAuthentication(BASIC_AUTH, "Nuki Hub", "You must log in.");
+                                           break;
+                                       case 1:
+                                           return this->_webServer->requestAuthentication(DIGEST_AUTH, "Nuki Hub", "You must log in.");
+                                           break;
+                                       case 2:
+                                           this->_webServer->sendHeader("Cache-Control",  "no-cache");
+                                           return this->redirect(this->_webServer, "/get?page=login", 302);
+                                           break;
+                                       case 3:
+                                       case 5:
+                                       case 4:
+                                       default:
+                                           break;
+                                   }
+                       
+                                   String message = "";
+                                   bool connected = processWiFi(this->_webServer, message);
+                                   buildConfirmHtml(this->_webServer, message, 10, true);
+                       
+                                   if(connected)
+                                   {
+                                       waitAndProcess(true, 3000);
+                                       restartEsp(RestartReason::ReconfigureWifi);
+                                   }
+                                   return; });
+
+        _webServer->on("/reboot", HTTP_GET, [this]()
+                           {
+                                   int authReq = doAuthentication(this->_webServer);
+                       
+                                   switch (authReq)
+                                   {
+                                       case 0:
+                                           return this->_webServer->requestAuthentication(BASIC_AUTH, "Nuki Hub", "You must log in.");
+                                           break;
+                                       case 1:
+                                           return this->_webServer->requestAuthentication(DIGEST_AUTH, "Nuki Hub", "You must log in.");
+                                           break;
+                                       case 2:
+                                            this->_webServer->sendHeader("Cache-Control",  "no-cache");
+                                            return this->redirect(this->_webServer, "/get?page=login", 302);
+                                           break;
+                                       case 3:
+                                       case 5:
+                                       case 4:
+                                       default:
+                                           break;
+                                   }
+                       
+                                   String value = "";
+                                   if (this->_webServer->hasArg("CONFIRMTOKEN")) // WebServer ersetzt PsychicRequest
+                                   {
+                                       value = this->_webServer->arg("CONFIRMTOKEN"); // Abrufen des Wertes aus dem GET/POST-Parameter
+                               
+                                       if (value == "")
+                                       {
+                                           buildConfirmHtml(this->_webServer, "No confirm code set.", 3, true);
+                                           return;
+                                       }
+                                   }
+                                   else
+                                   {
+                                       buildConfirmHtml(this->_webServer, "No confirm code set.", 3, true);
+                                       return;
+                                   }
+                       
+                                   if(value != _confirmCode)
+                                   {
+                                    this->_webServer->sendHeader("Cache-Control",  "no-cache");
+                                    return this->redirect(this->_webServer, "/", 302);
+                                   }
+                                   buildConfirmHtml(this->_webServer, "Rebooting...", 2, true);
+                                   waitAndProcess(true, 1000);
+                                   restartEsp(RestartReason::RequestedViaWebCfgServer);
+                                   return; });
+    }
+    else
+    {
+        
+    }
 
     // Eigentliches Starten des Webservers
     _webServer->begin();
@@ -67,10 +224,10 @@ void WebCfgServer::initialize()
     }
 }
 
-void WebCfgServer::handleNotFound()
+void WebCfgServer::redirect(WebServer *server, const char *url, int code)
 {
-    _webServer->sendHeader("Location", "/", true); // HTTP Redirect
-    _webServer->send(302, "text/plain", "Redirecting to /");
+    _webServer->sendHeader("Location", url, true); // HTTP Redirect
+    _webServer->send(code, "text/plain", "Redirecting to /");
 }
 
 void WebCfgServer::handleClient()
@@ -78,61 +235,222 @@ void WebCfgServer::handleClient()
     _webServer->handleClient();
 }
 
-void WebCfgServer::handleRoot()
+void WebCfgServer::waitAndProcess(const bool blocking, const uint32_t duration)
 {
-    // Einfaches HTML-Formular zum Setzen eines Preferences-Schlüssels
-    String html =
-        "<!DOCTYPE html>"
-        "<html>"
-        "<head><meta charset='utf-8'/><title>WebCfgServer</title></head>"
-        "<body>"
-        "<h2>WebCfgServer – Einstellungen</h2>"
-        "<form method='POST' action='/save'>"
-        "<label for='key'>Schlüssel:</label><br>"
-        "<input type='text' id='key' name='key' value='TestKey'/><br><br>"
-        "<label for='val'>Wert:</label><br>"
-        "<input type='text' id='val' name='val' value='HelloWorld'/><br><br>"
-        "<input type='submit' value='Speichern'/>"
-        "</form>"
-        "</body></html>";
-
-    _webServer->send(200, "text/html", html);
+    int64_t timeout = esp_timer_get_time() + (duration * 1000);
+    while(esp_timer_get_time() < timeout)
+    {
+        if(blocking)
+        {
+            delay(10);
+        }
+        else
+        {
+            vTaskDelay( 50 / portTICK_PERIOD_MS);
+        }
+    }
 }
 
-void WebCfgServer::handleSave()
+void WebCfgServer::buildConfirmHtml(WebServer *server, const String &message, uint32_t redirectDelay, bool redirect, String redirectTo)
 {
-    // Hier Werte aus dem POST-Request auslesen
-    if (_webServer->hasArg("key") && _webServer->hasArg("val"))
+    String response = "<html><head>";
+    String header;
+
+    if (!redirect)
     {
-        String key = _webServer->arg("key");
-        String val = _webServer->arg("val");
-
-        // In Preferences schreiben (Namespace muss außerhalb aufgerufen sein, z.B. preferences.begin("myNamespace", false);)
-        _preferences->putString(key.c_str(), val);
-
-        // Einfach eine Seite ausgeben, die bestätigt, was gespeichert wurde
-        String html =
-            "<!DOCTYPE html>"
-            "<html>"
-            "<head><meta charset='utf-8'/><title>Saved</title></head>"
-            "<body>"
-            "<h2>Gespeichert!</h2>"
-            "<p>Key: " +
-            key + "</p>"
-                  "<p>Value: " +
-            val + "</p>"
-                  "<a href='/'>Zur&uuml;ck</a>"
-                  "</body></html>";
-
-        _webServer->send(200, "text/html", html);
+        header = "<meta http-equiv=\"Refresh\" content=\"" + String(redirectDelay) + "; url=/\" />";
     }
     else
     {
-        _webServer->send(400, "text/plain", "Fehler: Bitte Key und Value angeben!");
+        header = "<script type=\"text/JavaScript\">function Redirect() { window.location.href = \"" + redirectTo + "\"; } setTimeout(function() { Redirect(); }, " + String(redirectDelay * 1000) + "); </script>";
     }
+
+    response += header;
+    response += "<title>Confirmation</title></head><body>";
+    response += message;
+    response += "</body></html>";
+
+    server->send(200, "text/html", response);
 }
 
-void WebCfgServer::handleCSS()
+void WebCfgServer::sendCss(WebServer *server)
 {
-    _webServer->send(200, "text/css", css);
+    // Setze den Cache-Control Header
+    server->sendHeader("Cache-Control", "public, max-age=3600");
+
+    // Setze den Content-Type auf text/css
+    server->setContentLength(CONTENT_LENGTH_UNKNOWN); // Länge muss nicht im Voraus bekannt sein
+    server->send(200, "text/css", css);               // Antwortstatus 200 und Content-Type "text/css"
+}
+
+String WebCfgServer::generateConfirmCode()
+{
+    int code = random(1000, 9999);
+    return String(code);
+}
+
+void WebCfgServer::buildSSIDListHtml(WebServer *server)
+{
+    _network->scan(true, false);
+    createSsidList();
+
+    String response = "<html><body><table>";
+
+    for (size_t i = 0; i < _ssidList.size(); i++)
+    {
+        response += "<tr class=\"trssid\" onclick=\"document.getElementById('inputssid').value = '" + _ssidList[i] + "';\">";
+        response += "<td colspan=\"2\">" + _ssidList[i] + " (" + String(_rssiList[i]) + " %)</td></tr>";
+    }
+
+    response += "</table></body></html>";
+
+    server->send(200, "text/html", response);
+}
+
+bool WebCfgServer::processWiFi(WebServer *server, String& message)
+{
+    bool res = false;
+    String ssid;
+    String pass;
+
+    for (uint8_t i = 0; i < server->args(); i++) // WebServer nutzt args()
+    {
+        String key = server->argName(i);
+        String value = server->arg(i);
+
+        if (key == "WIFISSID")
+        {
+            ssid = value;
+        }
+        else if (key == "WIFIPASS")
+        {
+            pass = value;
+        }
+        else if (key == "DHCPENA")
+        {
+            if (_preferences->getBool(preference_ip_dhcp_enabled, true) != (value == "1"))
+            {
+                _preferences->putBool(preference_ip_dhcp_enabled, (value == "1"));
+            }
+        }
+        else if (key == "IPADDR")
+        {
+            if (_preferences->getString(preference_ip_address, "") != value)
+            {
+                _preferences->putString(preference_ip_address, value);
+            }
+        }
+        else if (key == "IPSUB")
+        {
+            if (_preferences->getString(preference_ip_subnet, "") != value)
+            {
+                _preferences->putString(preference_ip_subnet, value);
+            }
+        }
+        else if (key == "IPGTW")
+        {
+            if (_preferences->getString(preference_ip_gateway, "") != value)
+            {
+                _preferences->putString(preference_ip_gateway, value);
+            }
+        }
+        else if (key == "DNSSRV")
+        {
+            if (_preferences->getString(preference_ip_dns_server, "") != value)
+            {
+                _preferences->putString(preference_ip_dns_server, value);
+            }
+        }
+        else if (key == "FINDBESTRSSI")
+        {
+            if (_preferences->getBool(preference_find_best_rssi, false) != (value == "1"))
+            {
+                _preferences->putBool(preference_find_best_rssi, (value == "1"));
+            }
+        }
+    }
+
+    ssid.trim();
+    pass.trim();
+
+    if (ssid.length() > 0 && pass.length() > 0)
+    {
+        if (_preferences->getBool(preference_ip_dhcp_enabled, true) && _preferences->getString(preference_ip_address, "").length() <= 0)
+        {
+            const IPConfiguration* _ipConfiguration = new IPConfiguration(_preferences);
+
+            if (!_ipConfiguration->dhcpEnabled())
+            {
+                WiFi.config(_ipConfiguration->ipAddress(), _ipConfiguration->dnsServer(), _ipConfiguration->defaultGateway(), _ipConfiguration->subnet());
+            }
+        }
+
+        WiFi.begin(ssid, pass);
+
+        int loop = 0;
+        while (!_network->isConnected() && loop < 150)
+        {
+            delay(100);
+            loop++;
+        }
+
+        if (!_network->isConnected())
+        {
+            message = "Failed to connect to the given SSID with the given secret key, credentials not saved<br/>";
+            return res;
+        }
+        else
+        {
+            message = "Connection successful. Rebooting Nuki Hub.<br/>";
+            _preferences->putString(preference_wifi_ssid, ssid);
+            _preferences->putString(preference_wifi_pass, pass);
+            res = true;
+        }
+    }
+    else
+    {
+        message = "No SSID or secret key entered, credentials not saved<br/>";
+    }
+
+    return res;
+}
+
+
+void WebCfgServer::createSsidList()
+{
+    int _foundNetworks = WiFi.scanComplete();
+    std::vector<String> _tmpSsidList;
+    std::vector<int> _tmpRssiList;
+
+    for (int i = 0; i < _foundNetworks; i++)
+    {
+        int rssi = constrain((100.0 + WiFi.RSSI(i)) * 2, 0, 100);
+        auto it1 = std::find(_ssidList.begin(), _ssidList.end(), WiFi.SSID(i));
+        auto it2 = std::find(_tmpSsidList.begin(), _tmpSsidList.end(), WiFi.SSID(i));
+
+        if (it1 == _ssidList.end())
+        {
+            _ssidList.push_back(WiFi.SSID(i));
+            _rssiList.push_back(rssi);
+            _tmpSsidList.push_back(WiFi.SSID(i));
+            _tmpRssiList.push_back(rssi);
+        }
+        else if (it2 == _tmpSsidList.end())
+        {
+            _tmpSsidList.push_back(WiFi.SSID(i));
+            _tmpRssiList.push_back(rssi);
+            int index = it1 - _ssidList.begin();
+            _rssiList[index] = rssi;
+        }
+        else
+        {
+            int index = it1 - _ssidList.begin();
+            int index2 = it2 - _tmpSsidList.begin();
+            if (_tmpRssiList[index2] < rssi)
+            {
+                _tmpRssiList[index2] = rssi;
+                _rssiList[index] = rssi;
+            }
+        }
+    }
 }
